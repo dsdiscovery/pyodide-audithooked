45e622a3b963f7cdd1dac2bb6c2faddfa57aef91
Garrett Credi <garrettcredi@gmail.com>
2024-04-25 00:47:15 -0500

    Python Audithook

--- src/core/main.c
+++ src/core/main.c
@@ -2,11 +2,80 @@
 #include "Python.h"
 #include <emscripten.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
 
 #define FAIL_IF_STATUS_EXCEPTION(status)                                       \
   if (PyStatus_Exception(status)) {                                            \
     goto finally;                                                              \
   }
+
+const char* allow_list[] = {
+  "builtins.id",
+  "code.__new__",
+  "compile",
+  "ctypes.dlopen",
+  "exec",
+  "gc.get_referents",
+  "import",
+  "marshal.loads",
+  "object.__delattr__",
+  "object.__getattr__",
+  "object.__setattr__",
+  "open",
+  "os.listdir",
+  "os.mkdir",
+  "os.putenv",
+  "os.remove",
+  "os.scandir",
+  "os.unsetenv",
+  "pathlib.Path.glob",
+  "shutil.unpack_archive",
+  "sys._getframe",
+  "sys._getframemodulename",
+  "sys.excepthook",
+  "tempfile.mkstemp",
+};
+const size_t allow_list_len = sizeof(allow_list) / sizeof(allow_list[0]);
+
+PyObject* whitelist_set = NULL;
+int
+hook_func(const char* event, PyObject* args, void* userData)
+{
+  if (!whitelist_set) {
+    whitelist_set = PySet_New(NULL);
+    for (size_t i = 0; i < allow_list_len; i++) {
+      PyObject* event = PyUnicode_FromString(allow_list[i]);
+      PySet_Add(whitelist_set, event);
+      Py_DECREF(event);
+    }
+    fflush(stderr);
+  }
+
+  bool malicious = false;
+
+  PyObject* event_obj = PyUnicode_FromString(event);
+
+  if (!event_obj) {
+    PyErr_SetString(PyExc_RuntimeError, "Failed to create event object");
+    return -1;
+  }
+
+  const int containment = PySet_Contains(whitelist_set, event_obj);
+  Py_DECREF(event_obj);
+  if (containment == -1) {
+    PyErr_SetString(PyExc_RuntimeError, "Failed to check containment");
+    return -1;
+  } else if (containment == 0) {
+    malicious = true;
+  }
+  if (malicious) {
+    PyErr_SetString(PyExc_RuntimeError, "EPERM: Operation not permitted");
+    return -1;
+  } else {
+    return 0;
+  }
+}
 
 // Initialize python. exit() and print message to stderr on failure.
 static void
@@ -31,6 +100,7 @@ initialize_python(int argc, char** argv)
   FAIL_IF_STATUS_EXCEPTION(status);
 
   config.write_bytecode = false;
+  PySys_AddAuditHook(hook_func, NULL);
   status = Py_InitializeFromConfig(&config);
   FAIL_IF_STATUS_EXCEPTION(status);
 
@@ -53,8 +123,8 @@ PyInit__pyodide_core(void);
  *     module
  *  3. Create a PyProxy wrapper around _pyodide package so that JavaScript can
  *     call into _pyodide._base.eval_code and
- *     _pyodide._import_hook.register_js_finder (this happens in loadPyodide in
- *     pyodide.js)
+ *     _pyodide._import_hook.register_js_finder (this happens in loadPyodide
+ * in pyodide.js)
  */
 int
 main(int argc, char** argv)
